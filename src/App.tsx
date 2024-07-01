d1205e210424260c4b9818e60c80baab2d31331f
rahulkumar2815 <rahulkumarchohan1234567@gmail.com>
2024-07-01 13:26:09 +0530

    text page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CompletedTask from './components/CompletedTask'
 import './App.scss';
 
 const App:React.FC = () => {
+  
   return (
     <>
   <div className="todo_app_section">
@@ -16,7 +17,7 @@ const App:React.FC = () => {
 
       </div>
       <div className="completed_task_list task_list">
-        <h2>CompletedListItem</h2>
+        <h2>Completed Task</h2>
       </div>
     </div>
   </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,23 +1,51 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './TaskList.scss'
+import { IInputField } from '../assets/interface/toDo';
+
 const TaskList:React.FC = () => {
+  const[inputFields,setInputFields]=useState<IInputField[]>([]);
+  useEffect (()=>{
+setInputFields([{value:""}]) 
+  },[])
+  const handleAddField=()=> {
+    if (inputFields[inputFields.length-1].value.trim()!=='')
+   
+    setInputFields([...inputFields,{value:""}]
+
+    );
+
+
+  }
+  const handleChange=(index:number,
+    event:React.ChangeEvent <HTMLInputElement>)=>{
+      let value=[...inputFields];
+      value[index].value=event.target.value;
+      setInputFields(value)
+      localStorage.setItem('taskList' ,JSON.stringify(value));
+      handleAddField();
+    }
+
+
   return (
     <>
-  <div className="task_list_section"> 
-    <div  className="task_list_item">
+  <div className="task_list_section">
+    {inputFields.map((item,index)=>( 
+    <div  className="task_list_item"key={index}>
       <input type="checkbox"
       title="check"
       className="task_checkbox" />
       <input type="text"
       title="text"
-      className="task_input"/>
+      className="task_input" value={item.value} onChange={(event)=>{handleChange(index,event)}}/>
       <button type="button"
       title="submit" >
         Remove
       </button>
 
     </div>
+    ))}
   </div>
+
     </>
   )
 }
